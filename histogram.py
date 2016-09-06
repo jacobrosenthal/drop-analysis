0dd91e8100482eb6252e91d3553186291e1b845f
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-09-06 01:22:28 -0400

    curve fit top and bottom seperately

--- histogram.py
+++ histogram.py
@@ -6,13 +6,14 @@ from scipy import ndimage as ndi
 import matplotlib.pyplot as plt
 from PIL import Image
 from skimage.color import label2rgb
+from skimage import feature
+import numpy.polynomial.polynomial as poly
 
 
 # L converts to 8-bit pixels, black and white
 # http://pillow.readthedocs.io/en/3.2.x/handbook/concepts.html#concept-modes
 im = np.array(Image.open('./Drop 1.png').convert('L'))
-print "im"
-print im
+# print "im, ", im
 
 line = 120 #based at looking at a histogram, how to get programmatically?
 
@@ -37,8 +38,8 @@ line = 120 #based at looking at a histogram, how to get programmatically?
 #then going above that can clean up some edges
 edges = im > 120 #looking at a histogram helps you see wehre most stuff is
 
-print "edges"
-print edges
+# print "edges, ", edges
+
 # now that we have some edges, fill holes for feature detection
 fillled = ndi.binary_fill_holes(edges)
 
@@ -55,86 +56,71 @@ sizes = np.bincount(label_objects.ravel())
 #another method, biggest is always first element
 mask_sizes = (sizes != sizes[0])
 
-print "mask_sizes"
-print mask_sizes
+# print "mask_sizes, ", mask_sizes
 
 #apply only the one feature back onto the label array
 single_feature = mask_sizes[label_objects]
 
-print "single feature"
-print single_feature
+# print "single feature, ", single_feature
 
 #overlay our single feature on the the original image
 image_label_overlay = label2rgb(single_feature, image=im)
 
 
 masked = np.bitwise_and(im, single_feature);
-
-print "masked"
-print masked
+# print "masked, ", masked
 
 
+# really dont understand why but when treated as points it seems rotated and flipped?
+flip_masked=np.rot90(masked)
+flip_masked=np.flipud(flip_masked)
+point_list=np.argwhere(flip_masked>0)
+# print "point_list, ", point_list
+
+#find the top curve by finding the largets y value for each x
+#not sure how to do this without an intermediate associative array topcurveDict
+topcurveDict = {}
+for i, val in enumerate(point_list):
+  x=val[0]
+  y=val[1]
+  if x not in topcurveDict or topcurveDict[x] > y:
+    topcurveDict[x]=y
+
+#turn topcurveDict back into a numpy array of numpy arrays of points
+top_point_list = np.array(topcurveDict.items(), dtype=int)
+x_list_top = top_point_list[:,0]
+y_list_top = top_point_list[:,1]
 
 
+#find the bot curve by finding the smallest y value for each x
+#not sure how to do this without an intermediate associative array topcurveDict
+botcurveDict = {}
+for i, val in enumerate(point_list):
+  x=val[0]
+  y=val[1]
+  if x not in botcurveDict or botcurveDict[x] < y:
+    botcurveDict[x]=y
 
-left = None
-top = None
-right = None
-bottom = None
-first = True
-it = np.nditer(masked, flags=['multi_index'])
-while not it.finished:
-  while 1:
-    if(it[0]):
-      # the first pixel we find is the default minima and maxima on both axes
-      if first:
-        left = it.multi_index
-        right = it.multi_index
-        bottom = it.multi_index
-        top = it.multi_index
-        first = False
-        break
-      y = it.multi_index[0]
-      x = it.multi_index[1]
-      # if x is less than left, its a new left
-      if(x<left[1]):
-        left = it.multi_index
-      # if x is larger than right, its a new right
-      if(x>right[1]):
-        right = it.multi_index
-      # if y is greater than bottom, its a new bottom
-      if(y>bottom[0]):
-        bottom = it.multi_index
-      # were starting from top so there cant be a more top point
-      break
-    break
-  it.iternext()
+#turn botcurveDict back into a numpy array of numpy arrays of points
+bot_point_list = np.array(botcurveDict.items(), dtype=int)
+x_list_bot = bot_point_list[:,0]
+y_list_bot = bot_point_list[:,1]
+
+# print "top_point_list, ", top_point_list
+# print "bot_point_list, ", bot_point_list
 
 
+# https://stackoverflow.com/questions/18767523/fitting-data-with-numpy
+coefs_top = poly.polyfit(x_list_top, y_list_top, 4)
+print "coefs_top", coefs_top
+ffit_top = poly.Polynomial(coefs_top)
 
 
-
-lefty = left[0]
-righty = right[0]
-#remember higher number, higher in photo
-print "left"
-print lefty
-print "right"
-print righty
+coefs_bot = poly.polyfit(x_list_bot, y_list_bot, 4)
+print "coefs_bot, ", coefs_bot
+ffit_bot = poly.Polynomial(coefs_bot)
 
 
-#how much angle to rotate for each point difference?
-#if lefty is higher, rotate left around center?
-# if(lefty>righty)
-#   ndi.interpolation.rotate(masked)
-
-#if righty is higher, rotate right around center?
-# if(righty>lefty)
-#   ndi.interpolation.rotate(masked)
-
-
-
-#find equation of an ellipse from 3 points?
 
 
 
@@ -159,38 +145,20 @@ ax3.set_title('filled')
 ax3.axis('off')
 ax3.set_adjustable('box-forced')
 
-
 ax4.imshow(image_label_overlay, interpolation='nearest')
 ax4.set_title('single feature overlayed')
 ax4.axis('off')
 ax4.set_adjustable('box-forced')
 
 
-ax5.imshow(masked, interpolation='nearest')
-ax5.set_title('masked')
-ax5.axis('off')
-ax5.set_adjustable('box-forced')
-circ = plt.Circle((top[1], top[0]), radius=10, color='b')
-circ2 = plt.Circle((bottom[1], bottom[0]), radius=10, color='b')
-circ3 = plt.Circle((right[1], right[0]), radius=10, color='g')
-circ4 = plt.Circle((left[1], left[0]), radius=10, color='g')
-ax5.add_patch(circ)  
-ax5.add_patch(circ2)  
-ax5.add_patch(circ3)  
-ax5.add_patch(circ4)  
+ax5.plot(x_list_top,y_list_top)
+ax5.plot(x_list_top,ffit_top(x_list_top))
 
-from matplotlib.patches import Ellipse
+ax5.plot(x_list_bot,y_list_bot)
+ax5.plot(x_list_bot,ffit_bot(x_list_bot))
 
-
-# r = top[1] - left[1]
-# c = plt.Circle((top[1], top[0]), r, color='r', linewidth=2, fill=False)
-# ax5.add_patch(c)
-
-
-ellipse = Ellipse(xy=(top[1], top[0]), width=0.036, height=0.012, 
-                        edgecolor='r', fc='None', lw=2)
-ax5.add_patch(ellipse)
-
+ax5.set_title('2 curve fit')
+# ax5.set_adjustable('box-forced')
 
 
 margins = dict(hspace=0.01, wspace=0.01, top=1, bottom=0, left=0, right=1)
